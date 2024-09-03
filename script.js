592e7aae01f079cd45752a61039be90a38d30de9
Jordan4184 <jordandonaldson14@gmail.com>
2024-09-03 10:55:34 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//Key Functionality
+
+//Add leader**
+    //Text box and button to add leader
+//Sort per leader**
+    //dropdown selector and button selector
+//Track wins
+    //2 buttons, W L
+//track win %
+    //Display calculation
+//store deck compilation**
+    //link leader to deck compilation
+//store sideboard
+    //display and store per leader
+//buttons to move card from sideboard to main and vice versa**
