14ae6cd85e0a7b5abc27050d287b7bbc4b62d5c4
Jordan4184 <jordandonaldson14@gmail.com>
2024-09-18 13:33:14 -0700

    Changed from factory function to class

--- script.js
+++ script.js
@@ -2,39 +2,43 @@
 
 //Track wins
 
-//Factory Function (obj) for "add leader button": 
-function addLeader(leader) {
-    return {
-        leader:leader, 
-        wins: [],
-        losses: [],
-        addWin: function(){
-            return this.wins.push(1);
-        },
-        addLoss: function(){
-            return this.losses.push(1);
-        },
-        totalWins: function(){
-            return this.wins.reduce((acc, val) => acc + val, 0);
-        },
-        totalLosses: function (){
-            return this.losses.reduce((acc, val) => acc + val, 0);
-        },
-        winLossRatio: function(){
-            const totalWins = this.totalWins();
-            const totalLosses = this.totalLosses();
-            return totalLosses > 0 ? ((totalWins / (totalWins + totalLosses))*100).toFixed(2) : totalWins * 100;
-        }, 
-    };
-};
+//Class for adding and tracking leader stats: 
 
-let vader = addLeader("Darth Vader")
+class Leader {
+    constructor(leaderName){
+        this.leaderName = leaderName; 
+        this.wins = [];
+        this.losses = []; 
+    }
+    addWin(){
+        return this.wins.push(1);
+    }
+    addLoss(){
+        return this.losses.push(1);
+    }
+    totalWins(){
+        return this.wins.reduce(function winSum(accumulator, currentValue){
+            return accumulator + currentValue; 
+        }, 0);
+    }
+    totalLosses(){
+        return this.losses.reduce(function lossSum(accumulator, currentValue){
+            return accumulator + currentValue
+        }, 0);
+    }
+    winLossRatio(){
+        return (((this.totalWins() / (this.totalWins() + this.totalLosses())) * 100).toFixed(2));
+    }
+}
+
+let vader = new Leader('Darth Vader');
+vader.addWin();
 vader.addWin();
 vader.addWin();
 vader.addLoss();
-console.log(vader.totalWins());
-console.log(vader.totalLosses());
-console.log(`You have a ${vader.winLossRatio()}% win rate`);
+console.log(vader.totalWins());  
+console.log(vader.totalLosses());  
+console.log(`Your win loss ratio is ${vader.winLossRatio()}%`); 
 
 //graph w/l per date
 
